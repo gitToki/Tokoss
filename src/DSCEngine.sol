083baa47f96511a84376370a6e07374455e00209
Toki <yassineferhane69001@hotmail.com>
2024-12-17 22:28:25 +0100

    Constructor soon finish, first function RedeemCollateral in progress

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -1,19 +1,39 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import {TokusdStablecoin} from "./TokusdStablecoin.sol";
+
 contract DSCEngine {
     error DSCEngine_ZeroFundsTransaction();
+    error DSCEngine_TokenAddressAndPriceFeedNotSameLength();
+    error DSCGEngin_TokenNotAllowed();
 
+    mapping(address token => address priceFeeds) private s_priceFeeds;
+
+    TokusdStablecoin private immutable i_dsc;
 
     modifier moreThanZero(uint256 amount) {
         if (amount <= 0){
-            revert DSCEngine_ZeroFundsTransaction;
+            revert DSCEngine_ZeroFundsTransaction();
         }
         _;
     }
- //   modifier isAllowedToken(address token){
-        
-//    }
+   modifier isAllowedToken(address token){
+        if (s_priceFeeds[token] == address(0)){
+            revert DSCGEngin_TokenNotAllowed();
+        }
+        _;
+    }
+
+    constructor(address[] memory tokenAddresses, address[] memory priceFeedAddress, address dscAddress){
+        if (tokenAddresses.length != priceFeedAddress.length) {
+            revert DSCEngine_TokenAddressAndPriceFeedNotSameLength();
+        }
+        for (uint256 i = 0; i< tokenAddresses.length; i++){
+            s_priceFeeds[tokenAddresses[i]] = priceFeedAddress[i];
+        }
+        i_dsc = TokusdStablecoin(dscAddress);
+    }
 
     function DepositCollateralAndMintDSC() external{}
     function RedeemCollateralForDSC() external{}
